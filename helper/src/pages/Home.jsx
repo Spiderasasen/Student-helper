4ff6f9cc66ca78d952faa7b6c01fa7458f7afbb0
spiderasasen <spiderasasen@gmail.com>
2026-04-03 10:55:16 -0400

    making the home page
    Made the basic skeleton
    - Upload a syllabus
      - when clicked it will take the user to upload there syllabus, which will later see what the users weight of the class and will then send them to a what if section
    - GPA predictor
      - when clicked, it will send the user to enter there GPA and also the amount of credits of classes that will be given to them. then sending them to another section of a what if something happens. like failing a class

--- helper/src/pages/Home.jsx
+++ helper/src/pages/Home.jsx
@@ -1,6 +1,21 @@
 function Home(){
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+
+            {/*main buttons*/}
+            <div>
+                {/*uploading a syllabus*/}
+                <div>
+                    <h4>Upload a Syllabus</h4>
+                </div>
+
+                {/*GPA Predictor*/}
+                <div>
+                    <h4>GPA Predictor</h4>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
