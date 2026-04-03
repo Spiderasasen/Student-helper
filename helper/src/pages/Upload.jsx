e15d288baf0141765b824c771826b4b446786a7b
spiderasasen <spiderasasen@gmail.com>
2026-04-03 16:12:42 -0400

    making Upload.jsx
    fixed her up

--- helper/src/pages/Upload.jsx
+++ helper/src/pages/Upload.jsx
@@ -1,23 +1,22 @@
 import "../styles/main.css"
 import "../styles/upload.css"
 
-function Upload(){
-    return(
-        <div className="upload-conatiner">
-            {/*Title area*/}
+function Upload() {
+    return (
+        <div className="upload-container">
+
+            {/* Title Section */}
             <div className="upload-header">
                 <h1>Upload a Syllabus</h1>
-                {/*subheader*/}
                 <p className="upload-subtitle">
-                   Please upload your syllabus so we can extract the grading weight
-                    and generate accurate predictions for your class performance
+                    Please upload your syllabus so we can extract the grading weights
+                    and generate accurate predictions for your class performance.
                 </p>
             </div>
 
-            {/*Upload area*/}
+            {/* Upload Area */}
             <div className="upload-area">
 
-                {/*uploading a file*/}
                 <div className="upload-box">
                     <p className="upload-instructions">Choose your syllabus file</p>
                     <input
@@ -27,16 +26,18 @@ function Upload(){
                     />
                 </div>
 
-                {/*submit button*/}
+                {/* Submit Button */}
                 <div className="upload-submit">
                     <button className="upload-button" disabled>
                         Continue
                     </button>
                     <p className="upload-note">Button activates once a file is selected</p>
                 </div>
+
             </div>
 
         </div>
     )
 }
+
 export default Upload;
